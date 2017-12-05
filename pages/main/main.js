442606fb872e62bc03d0ea357179eb4cf3875daf
zhouvsjian <zhouvsjian@126.com>
2017-12-05 16:24:44 +0800

    角色菜单筛选

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,17 +1,30 @@
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    menuData:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    switch(app.globalData.roleid){
+      case '7'://学生
+        menuData = [{name:'个人中心',url:''},{name:'实习登记',url:''},{name:'签到请假',url:''},{name:'简历管理',url:''},{name:'查找岗位',url:''},
+                  {name:'职位申请',url:''},{name:'消息中心',url:''},{name:'任务列表',url:''},{name:'报告管理',url:''}];
+        break;
+      case '4'://辅导员
+      case '9'://班主任
+        break;
+      case '3'://导师
+        break;
+      case '6'://师傅
+        break;
+    }
   },
 
   /**
